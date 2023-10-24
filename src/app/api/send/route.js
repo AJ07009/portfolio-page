b51406e3a841b038c5f0617e661e8fd4c86940a7
Aidan Josias <52139870+AJ07009@users.noreply.github.com>
2023-10-24 14:02:45 +0200

    template

--- src/app/api/send/route.js
+++ src/app/api/send/route.js
@@ -15,10 +15,96 @@ export async function POST(req, res) {
         subject: subject,
         react: (
         <>
-            <h1>{subject}</h1>
-            <p>Thank you for contacting us!</p>
-            <p>New message submitted:</p>
-            <p>{message}</p>
+        <Html>
+        <Head />
+        <Preview>{subject}</Preview>
+        <Body style={main}>
+        <Container>
+            <Section style={logo}>
+            <Img src={`${baseUrl}/static/yelp-logo.png`} />
+            </Section>
+
+            <Section style={content}>
+            <Img width={620} src={`${baseUrl}/static/yelp-header.png`} />
+
+            <Row style={{ ...boxInfos, paddingBottom: '0' }}>
+                <Column>
+                <Heading
+                    style={{
+                    fontSize: 32,
+                    fontWeight: 'bold',
+                    textAlign: 'center',
+                    }}
+                >
+                    Hi {userFirstName},
+                </Heading>
+                <Heading
+                    as="h2"
+                    style={{
+                    fontSize: 26,
+                    fontWeight: 'bold',
+                    textAlign: 'center',
+                    }}
+                >
+                    We noticed a recent login to your Yelp account.
+                </Heading>
+
+                <Text style={paragraph}>
+                    <b>Time: </b>
+                    {formattedDate}
+                </Text>
+                <Text style={{ ...paragraph, marginTop: -5 }}>
+                    <b>Device: </b>
+                    {loginDevice}
+                </Text>
+                <Text style={{ ...paragraph, marginTop: -5 }}>
+                    <b>Location: </b>
+                    {loginLocation}
+                </Text>
+                <Text
+                    style={{
+                    color: 'rgb(0,0,0, 0.5)',
+                    fontSize: 14,
+                    marginTop: -5,
+                    }}
+                >
+                  *Approximate geographic location based on IP address:
+                    {loginIp}
+                </Text>
+
+                <Text style={paragraph}>
+                    If this was you, there's nothing else you need to do.
+                </Text>
+                <Text style={{ ...paragraph, marginTop: -5 }}>
+                    If this wasn't you or if you have additional questions, please
+                    see our support page.
+                </Text>
+                </Column>
+            </Row>
+            <Row style={{ ...boxInfos, paddingTop: '0' }}>
+                <Column style={containerButton} colSpan={2}>
+                <Button style={button}>Learn More</Button>
+                </Column>
+            </Row>
+            </Section>
+
+            <Section style={containerImageFooter}>
+            <Img width={620} src={`${baseUrl}/static/yelp-footer.png`} />
+            </Section>
+
+            <Text
+            style={{
+                textAlign: 'center',
+                fontSize: 12,
+                color: 'rgb(0,0,0, 0.7)',
+            }}
+            >
+            © 2022 | Yelp Inc., 350 Mission Street, San Francisco, CA 94105,
+            U.S.A. | www.yelp.com
+            </Text>
+        </Container>
+        </Body>
+    </Html>
         </>
         ),
     });
