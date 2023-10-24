1c3afbb2169370e40536c8fbc2734858e4b7dc32
Aidan Josias <52139870+AJ07009@users.noreply.github.com>
2023-10-24 14:11:52 +0200

    q

--- src/app/api/send/route.js
+++ src/app/api/send/route.js
@@ -1,6 +1,7 @@
 // import { EmailTemplate } from '../../../components/EmailTemplate';
 import { NextResponse } from "next/server";
 import { Resend } from "resend";
+import YelpRecentLoginEmail from "@/app/emails/template";
 
 const resend = new Resend(process.env.RESEND_API_KEY);
 const fromEmail = process.env.FROM_EMAIL;
@@ -13,17 +14,17 @@ export async function POST(req, res) {
         from: fromEmail,
         to: [fromEmail, email],
         subject: subject,
-        react: (
-        <>
-            <h1>{subject}</h1>
-            <p>Thank you for contacting us!</p>
-            <p>New message submitted:</p>
-          < p>{message}</p>
-        </>
-        ),
+        react: YelpRecentLoginEmail()
+        // <>
+        //     <h1>{subject}</h1>
+        //     <p>Thank you for contacting us!</p>
+        //     <p>New message submitted:</p>
+        // < p>{message}</p>
+        // </>
+        // ),
     });
     return NextResponse.json(data);
-  } catch (error) {
+    } catch (error) {
     return NextResponse.json({ error });
-  }
+    }
 }
